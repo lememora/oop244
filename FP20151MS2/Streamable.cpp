8b82a54c3eb801bfee12a750b4f757e6ff14691e
Rafael Moraes <contact@moraesrafael.com>
2015-04-04 11:05:10 -0400

    draft

--- FP20151MS2/Streamable.cpp
+++ FP20151MS2/Streamable.cpp
@@ -1,11 +1,37 @@
+#include <iostream>
 #include "Streamable.h"
 namespace oop244{
   // destructor:
 
+  Streamable::~Streamable() { }
+
+  std::fstream& Streamable::Store(std::fstream& file) const {
+    return file;
+  }
+
+  std::fstream& Streamable::Load(std::fstream& file) const {
+    return file;
+  }
+
+  std::ostream& Streamable::display(std::ostream& os, bool linear) const {
+    return os;
+  }
+
+  std::istream& Streamable::conInput(std::istream& is) const {
+    return is;
+  }
 
 
   // Non-memenber operator overoad implementation for cin and cout:
 
+  std::ostream& operator<<(std::ostream& os, const Streamable& S) {
+    // return S.Store(os);
+    return os;
+  }
 
+  std::istream& operator>>(std::istream& is, Streamable& S) {
+    // return S.Load(is);
+    return is;
+  }
 
 }
--- FP20151MS2/Streamable.h
+++ FP20151MS2/Streamable.h
@@ -8,14 +8,22 @@ namespace oop244{
   public:
     // pure virutal methods:
 
+    // virtual void draw() const = 0;  // = 0 means it is "pure virtual"
 
-
+    virtual std::fstream& Store(std::fstream& file) const = 0;
+    virtual std::fstream& Load(std::fstream& file) const = 0;
+    virtual std::ostream& display(std::ostream& os, bool linear) const = 0;
+    virtual std::istream& conInput(std::istream& is) const = 0;
 
     // virutal destructor:
+
+    virtual ~Streamable();
 
   };
   // Non-memenber operator overoad prototypes for cin and cout:
 
+  std::ostream& operator<<(std::ostream& os, const Streamable& D);
+  std::istream& operator>>(std::istream& is, Streamable& D);
 
 }
 #endif
