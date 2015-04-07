5573f353d2033d390796f54e57fe96ebe022d0a5
Rafael Moraes <contact@moraesrafael.com>
2015-04-07 15:30:44 -0400

    added streamable from ms2

--- FP20151MS3/Streamable.cpp
+++ FP20151MS3/Streamable.cpp
@@ -1,7 +1,30 @@
+// OOP244 Final Project Milestone 3, 20152
+// File Streamable.cpp
+// Version 1.0
+// Date 2015/04/04
+// Author   Rafael Moraes
+// Description
+// Streamable class
+//
+// Revision History
+// -----------------------------------------------------------
+// Name         Streamable        Reason
+// R.Moraes     2015/04/04        Assignment completion
+////////////////////////////////////////////////////////////////
 #include "Streamable.h"
 namespace oop244{
+  // destructor:
 
+  Streamable::~Streamable() { }
 
+  // Non-memenber operator overoad implementation for cin and cout:
 
+  std::ostream& operator<<(std::ostream& os, const Streamable& S) {
+    return S.display(os, true);
+  }
+
+  std::istream& operator>>(std::istream& is, Streamable& S) {
+    return S.conInput(is);
+  }
 
 }
