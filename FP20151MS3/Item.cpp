f180c48615df2866fcfecd0be0a9e56097b8ce4e
Rafael Moraes <contact@moraesrafael.com>
2015-04-10 18:51:54 -0400

    first draft

--- FP20151MS3/Item.cpp
+++ FP20151MS3/Item.cpp
@@ -1,39 +1,147 @@
+// OOP244 Final Project Milestone 3, 20152
+// File Item.cpp
+// Version 1.0
+// Date 2015/04/10
+// Author   Rafael Moraes
+// Description
+// Item class
+//
+// Revision History
+// -----------------------------------------------------------
+// Name         Streamable        Reason
+// R.Moraes     2015/04/10        Assignment completion
+////////////////////////////////////////////////////////////////
+
 // header file includes
-#include <Item.h>
+#include "Item.h"
 
-namespace oop244{
+using namespace std;
 
-  // constructors 
-  Item::Item(char[] upc, char* name, double price, int quantity, int taxed) {
+namespace oop244 {
+
+  // constructors
+  Item::Item(const char upc[], const char* name, double price, int qtyNeeded, bool taxed) {
+    strncpy(_upc, upc, MAX_UPC_LEN);
+
+    if (_name){
+      delete[] _name;
+      _name = (char*)0;
+    }
+    _name = new (nothrow) char[strlen(name)];
+    strncpy(_name, name, strlen(name));
+
+    _price = price;
+    _qtyNeeded = qtyNeeded;
+    _taxed = taxed;
+
+    _quantity = 0;
   }
 
+  Item::Item(const Item& I) {
+    *this = I;
+  }
 
-  // operator=
+  Item& Item::operator=(const Item& I) {
+    if (this != &I) {
+      strcpy(_upc, I._upc);
+      _quantity = I._quantity;
+      _price = I._price;
+      _qtyNeeded = I._qtyNeeded;
+      _taxed = I._taxed;
 
+      if (_name){
+        delete[] _name;
+        _name = (char*)0;
+      }
 
+      _name = new (nothrow) char[strlen(I._name)];
+      strncpy(_name, I._name, strlen(I._name));
+    }
+    return *this;
+  }
 
+  void Item::upc(char upc[]) {
+    if (upc) {
+      strncpy(_upc, upc, MAX_UPC_LEN);
+    }
+  }
 
-  // setters 
+  void Item::name(char* name) {
+    if (_name){
+      delete[] _name;
+      _name = (char*)0;
+    }
+    _name = new (nothrow) char[strlen(name)];
+    strncpy(_name, name, strlen(name));
+  }
 
+  void Item::price(double price) {
+    _price = price;
+  }
 
+  void Item::taxed(bool taxed) {
+    _taxed = taxed;
+  }
 
+  void Item::quantity(int quantity) {
+    _quantity = quantity;
+  }
 
+  void Item::qtyNeeded(int qtyNeeded) {
+    _qtyNeeded = qtyNeeded;
+  }
 
+  const char* Item::upc() const {
+    return _upc;
+  }
 
+  const char* Item::name() const {
+    return _name;
+  }
 
+  double Item::price() const {
+    return _price;
+  }
 
+  bool Item::taxed() const {
+    return _taxed;
+  }
 
+  int Item::quantity() const {
+    return _quantity;
+  }
 
-  // getters
+  int Item::qtyNeeded() const {
+    return _qtyNeeded;
+  }
 
+  double Item::cost() const {
+    double cost = _price;
+    if (_taxed) {
+      cost*= TAX + 1;
+    }
+    return cost;
+  }
 
-  // member operator overloads 
+  // operator overloads
+  bool Item::operator==(const char* upc) {
+    return strcmp(upc, _upc)==0;
+  }
 
+  int Item::operator+=(int n) {
+    return _quantity+=n;
+  }
 
-  // non-member operator overload
-
-
+  double operator+=(double &n, const Item& I) {
+    return n+=(I.cost() * I.quantity());
+  }
 
   // destructor
+  Item::~Item() {
+    if (_name) {
+      delete[] _name;
+      _name = (char*)0;
+    }
+  }
 
 }
--- FP20151MS3/Item.h
+++ FP20151MS3/Item.h
@@ -1,51 +1,68 @@
+// OOP244 Final Project Milestone 3, 20152
+// File Item.h
+// Version 1.0
+// Date 2015/04/10
+// Author   Rafael Moraes
+// Description
+// Item header file
+//
+// Revision History
+// -----------------------------------------------------------
+// Name         Streamable        Reason
+// R.Moraes     2015/04/10        Assignment completion
+////////////////////////////////////////////////////////////////
+
 #ifndef __244_ITEM__
 #define __244_ITEM__
 
 // header file includes
+#include "Streamable.h"
+#include "general.h"
 
 namespace oop244 {
-
   class Item : public Streamable {
   private:
-    char _upc[MAX_UPC_LEN+1];
-    char *_name;
+    char _upc[MAX_UPC_LEN + 1];
+    char* _name;
     double _price;
-    int _taxed;
+    bool _taxed;
     int _quantity;
     int _qtyNeeded;
 
   public:
     // constructors
-    Item(char[] upc, char* name, double price, int quantity, int taxed);
+    Item(const char upc[], const char* name, double price, int quantity, bool taxed=true);
+    Item(const Item& I);
 
     // destructor
-
+    ~Item();
 
     // setters
-
-
-
-
-
-
-
+    void upc(char upc[]);
+    void name(char* name);
+    void price(double price);
+    void taxed(bool taxed);
+    void quantity(int quantity);
+    void qtyNeeded(int qtyNeeded);
 
     // getters
+    const char* upc() const;
+    double price() const;
+    const char* name() const;
+    bool taxed() const;
+    int quantity() const;
+    int qtyNeeded() const;
 
-
-
-
-
-
+    double cost() const;
 
     // operators
-
-
-
+    Item& operator=(const Item& I);
+    bool operator==(const char* upc);
+    int operator+=(int n);
   };
+
   // Non-member operator overload prototype
   double operator+=(double& d, const Item& I);
 }
-
 
 #endif
