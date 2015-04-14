ca2a5077c97d4845ff6438ff0a95c9c6253d0adc
Rafael Moraes <contact@moraesrafael.com>
2015-04-14 12:08:04 -0400

    added Date class

--- FP20151MS5/Date.cpp
+++ FP20151MS5/Date.cpp
@@ -1,29 +1,53 @@
-#include "date.h"
+// OOP244 Final Project Milestone 1, 20151
+// File Date.cpp
+// Version 1.0
+// Date 2015/04/02
+// Author   Rafael Moraes
+// Description
+// Date class
+//
+// Revision History
+// -----------------------------------------------------------
+// Name         Date        Reason
+// R.Moraes     2015/04/02  Assignment completion
+////////////////////////////////////////////////////////////////
+
+#include "Date.h"
 #include "general.h"
 #include <iomanip>
 #include <iostream>
+#include <sstream>
+#include <cstring>
+#include <cstdio>
+
 using namespace std;
-namespace oop244{
+
+namespace oop244 {
   // returns a uniqe value out of date
   // this value is used to compare two dates
-  int Date::value()const{
+  int Date::value() const {
     return _year * 372 + _mon * 31 + _day;
   }
 
-  // validate(), this is an optional private function to write
-  // for validation logic and setting the _readErrorCode.
-  // see the read() funciton description for more detail
+  // validate function
+  void Date::validate() {
+    if (_year < MIN_YEAR || _year > MAX_YEAR) {
+      errCode(YEAR_ERROR);
+    }
+    if (_mon < 1 || _mon > 12) {
+      errCode(MON_ERROR);
+    // mdays() depends on a valid _mon
+    } else if (_day < 1 || _day > mdays()) {
+      errCode(DAY_ERROR);
+    }
+  }
 
-
-
-
-  // mday():
   // returns the day of the month.
   // _mon value must be set for this to work
   // if _mon is invalid, this function returns -1
   // leap years are considered in this logic
 
-  int Date::mdays()const{
+  int Date::mdays() const {
     int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, -1 };
     int mon = _mon >= 1 && _mon <= 12 ? _mon : 13;
     mon--;
@@ -32,26 +56,121 @@ namespace oop244{
 
   // constructors
 
+  Date::Date() {
+    _year = 0;
+    _mon = 0;
+    _day = 0;
+    errCode(NO_ERROR);
+  }
 
+  Date::Date(int year, int month, int day) {
+    _year = year;
+    _mon = month;
+    _day = day;
+    errCode(NO_ERROR);
+    validate();
+  }
 
   // member fucntions
 
+  bool Date::bad() const {
+    return errCode() != NO_ERROR;
+  }
 
+  void Date::errCode(int errorCode) {
+    _readErrorCode = errorCode;
+  }
 
-
+  int Date::errCode() const {
+    return _readErrorCode;
+  }
 
   // operators
 
+  bool Date::operator==(const Date& D) const {
+    return this->value() == D.value();
+  }
 
+  bool Date::operator!=(const Date& D) const {
+    return this->value() != D.value();
+  }
 
+  bool Date::operator<(const Date& D) const {
+    return this->value() < D.value();
+  }
 
+  bool Date::operator>(const Date& D) const {
+    return this->value() > D.value();
+  }
+
+  bool Date::operator<=(const Date& D) const {
+    return this->value() <= D.value();
+  }
+
+  bool Date::operator>=(const Date& D) const {
+    return this->value() >= D.value();
+  }
 
   // IO funtions
 
+  std::istream& Date::read(std::istream& istr) {
+    int n;
+    char c;
 
+    // empty safe state
+    _year = 0;
+    _mon = 0;
+    _day = 0;
+    errCode(NO_ERROR);
+
+    if (istr >> n && !istr.fail()) {
+      _year = n;
+    } else {
+      errCode(CIN_FAILED);
+    }
+
+    if (istr >> c && istr.fail()) {
+      errCode(CIN_FAILED);
+    }
+
+    if (istr >> n && !istr.fail()) {
+      _mon = n;
+    } else {
+      errCode(CIN_FAILED);
+    }
+
+    if (istr >> c && istr.fail()) {
+      errCode(CIN_FAILED);
+    }
+
+    if (istr >> n && !istr.fail()) {
+      _day = n;
+    } else {
+      errCode(CIN_FAILED);
+    }
+
+    if (!bad()) {
+      validate();
+    }
+
+    return istr;
+  }
+
+  std::ostream& Date::write(std::ostream& ostr) const {
+    return ostr << std::right
+                << std::setfill('0') << std::setw(2) << _year << '/'
+                << std::setfill('0') << std::setw(2) << _mon  << '/'
+                << std::setfill('0') << std::setw(2) << _day;
+  }
 
   // non-memeber operator overloads
 
+  std::ostream& operator<<(std::ostream& os, const Date& D) {
+    return D.write(os);
+  }
 
+  std::istream& operator>>(std::istream& is, Date& D) {
+    return D.read(is);
+  }
 
 }
