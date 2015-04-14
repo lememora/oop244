23dd8f0bf1217d0de4bb2e1dfb757fb9ee2d5bac
Rafael Moraes <contact@moraesrafael.com>
2015-04-13 22:04:39 -0400

    implementing perishable

--- FP20151MS4/Perishable.cpp
+++ FP20151MS4/Perishable.cpp
@@ -1,5 +1,20 @@
+// OOP244 Final Project Milestone 4, 20151
+// File Perishable.cpp
+// Version 1.0
+// Date 2015/04/13
+// Author   Rafael Moraes
+// Description
+// Perishable class
+//
+// Revision History
+// -----------------------------------------------------------
+// Name         Date              Reason
+// R.Moraes     2015/04/13        Assignment completion
+////////////////////////////////////////////////////////////////
+
 #include <fstream>
 #include <cstring>
+#include <iomanip>
 #include "Perishable.h"
 
 using namespace std;
@@ -10,17 +25,12 @@ namespace oop244 {
     unit("");
   }
 
-  void Perishable::unit(const char unit_[]) {
-    if (unit_) {
-      strncpy(_unit, unit_, 11);
-    }
-    if (strlen(unit_) >= 11) {
-      _unit[10] = '\0';
-    }
+  void Perishable::unit(const char unit[]) {
+    strncpy(_unit, unit, 11);
   }
 
-  void Perishable::expiry(const Date expiry_) {
-    _expiry = expiry_;
+  void Perishable::expiry(const Date &expiry) {
+    _expiry = expiry;
   }
 
   const char* Perishable::unit() const {
@@ -32,18 +42,128 @@ namespace oop244 {
   }
 
   fstream& Perishable::store(fstream& file) const {
+    file << 'P' << ',' << upc() << ',' << name() << ',' << price() << ',' << unit() << ',' << quantity() << ',' << qtyNeeded() << ',' << expiry() << ',' << endl;
     return file;
   }
 
   fstream& Perishable::load(fstream& file) {
+    char sbuf[SBUF_LEN];
+    char cbuf;
+    double dbuf;
+    int ibuf;
+    Date datebuf;
+
+    file.getline(sbuf, SBUF_LEN, ',');
+    upc(sbuf);
+    file.getline(sbuf, SBUF_LEN, ',');
+    name(sbuf);
+    file >> dbuf >> cbuf;
+    price(dbuf);
+    file.getline(sbuf, SBUF_LEN, ',');
+    unit(sbuf);
+    file >> ibuf >> cbuf;
+    quantity(ibuf);
+    file >> ibuf >> cbuf;
+    qtyNeeded(ibuf);
+    file >> datebuf >> cbuf;
+    expiry(datebuf);
+
     return file;
   }
 
   ostream& Perishable::display(ostream& os, bool linear) const {
+    if (_err.isClear()==false) {
+      os << _err.message();
+    } else if (linear) {
+      os <<
+        setfill(' ') << left << setw(MAX_UPC_LEN) << upc() << '|' <<
+        setw(20) << name() << '|' <<
+        right << setw(7) << fixed << showpoint << setprecision(2) << cost() << '|' <<
+        setw(4) << quantity() << '|' <<
+        qtyNeeded();
+    } else {
+      os <<
+        "Upc: "              << upc()       << endl <<
+        "Name: "             << name()      << endl <<
+        "Price: "            << price()     << endl <<
+        "Price after tax: "  << cost()      << endl <<
+        "Quantity On Hand: " << quantity()  << endl <<
+        "Quantity Needed: "  << qtyNeeded() << endl <<
+        "Unit: "             << qtyNeeded() << endl <<
+        "Expiry date: "      << qtyNeeded() << endl;
+    }
     return os;
   }
 
   istream& Perishable::conInput(istream& is) {
+    char sbuf[256];
+    double dbuf;
+    int ibuf;
+    Date datebuf;
+
+    if (is.fail()==false) {
+      _err.clear();
+      cout << "Perishable Entry: " << endl;
+
+      cout << "Upc: ";
+      is >> sbuf;
+      upc(sbuf);
+
+      cout << "Price: ";
+      is >> dbuf;
+      if (cin.fail()) {
+        _err.message("Invalid Price Entry");
+      } else {
+        price(dbuf);
+      }
+
+      if (_err.isClear()) {
+        cout << "Quantity On hand: ";
+        is >> ibuf;
+        if (cin.fail()) {
+          _err.message("Invalid Quantity Entry");
+        } else {
+          quantity(ibuf);
+        }
+      }
+
+      if (_err.isClear()) {
+        cout << "Quantity Needed: ";
+        is >> ibuf;
+        if (cin.fail()) {
+          _err.message("Invalid Quantity Needed Entry");
+        } else {
+          qtyNeeded(ibuf);
+        }
+      }
+
+      cout << "Unit: ";
+      is >> sbuf;
+      upc(sbuf);
+
+      if (_err.isClear()) {
+
+        cout << "Expiry date (YYYY/MM/DD) : ";
+        Date datebuf;
+        is >> datebuf;
+
+        if (datebuf.bad()) {
+          if (_expiry.errCode()==CIN_FAILED) {
+            _err.message("Invalid Date Entry");
+          } else if (_expiry.errCode()==YEAR_ERROR) {
+            _err.message("Invalid Year in Date Entry");
+          } else if (_expiry.errCode()==MON_ERROR) {
+            _err.message("Invalid Month in Date Entry");
+          } else if (_expiry.errCode()==DAY_ERROR) {
+            _err.message("Invalid Day in Date Entry");
+          }
+          is.setstate(ios::failbit);
+        } else {
+          expiry(datebuf);
+        }
+      }
+    }
+
     return is;
   }
 
--- FP20151MS4/Perishable.h
+++ FP20151MS4/Perishable.h
@@ -1,3 +1,17 @@
+// OOP244 Final Project Milestone 4, 20151
+// File Perishable.h
+// Version 1.0
+// Date 2015/04/13
+// Author   Rafael Moraes
+// Description
+// Perishable header file
+//
+// Revision History
+// -----------------------------------------------------------
+// Name         Date              Reason
+// R.Moraes     2015/04/13        Assignment completion
+////////////////////////////////////////////////////////////////
+
 #ifndef __244_Perishable_H__
 #define __244_Perishable_H__
 
@@ -5,7 +19,10 @@
 #include "Item.h"
 #include "ErrorMessage.h"
 
+#define SBUF_LEN 256
+
 namespace oop244 {
+
   class Perishable : public Item {
   private:
     ErrorMessage _err;
@@ -20,14 +37,13 @@ namespace oop244 {
     std::ostream& display(std::ostream& os, bool linear) const;
     std::istream& conInput(std::istream& is);
 
-    void unit(const char unit_[]);
-    void expiry(const Date expiry_);
+    void unit(const char unit[]);
+    void expiry(const Date &expiry);
 
     const char* unit() const;
     Date expiry() const;
-
   };
-}
 
+}
 
 #endif
