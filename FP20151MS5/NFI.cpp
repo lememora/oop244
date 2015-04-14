ffdf07e207ad9772a2583949f22502c49a08ae46
Rafael Moraes <contact@moraesrafael.com>
2015-04-14 12:11:24 -0400

    added NFI class

--- FP20151MS5/NFI.cpp
+++ FP20151MS5/NFI.cpp
@@ -1,25 +1,121 @@
-//includes go here
+// OOP244 Final Project Milestone 4, 20151
+// File NFI.cpp
+// Version 1.0
+// Date 2015/04/13
+// Author   Rafael Moraes
+// Description
+// Non-Food Item class
+//
+// Revision History
+// -----------------------------------------------------------
+// Name         Date              Reason
+// R.Moraes     2015/04/13        Assignment completion
+////////////////////////////////////////////////////////////////
 
+#include <iomanip>
+#include "NFI.h"
+
+using namespace std;
 
 namespace oop244{
-  // constructor
 
+  NFI::NFI() : Item("", "", 0, 1) {
+  }
 
-    
-  // fstream& NFI::store(fstream& file)const
+  fstream& NFI::store(fstream& file) const {
+    file << 'N' << ',' << upc() << ',' << name() << ',' << price() << ',' << quantity() << ',' << qtyNeeded() << endl;
+    return file;
+  }
 
+  fstream& NFI::load(fstream& file) {
+    char sbuf[SBUF_LEN];
+    char cbuf;
+    double dbuf;
+    int ibuf;
 
+    file.getline(sbuf, SBUF_LEN, ',');
+    upc(sbuf);
+    file.getline(sbuf, SBUF_LEN, ',');
+    name(sbuf);
+    file >> dbuf >> cbuf;
+    price(dbuf);
+    file >> ibuf >> cbuf;
+    quantity(ibuf);
+    file >> ibuf >> cbuf;
+    qtyNeeded(ibuf);
 
-  // fstream& NFI::load(fstream& file)
+    return file;
+  }
 
-    
-    
-  // ostream& NFI::display(ostream& ostr, bool linear)const
+  ostream& NFI::display(ostream& os, bool linear) const {
+    if (_err.isClear()==false) {
+      os << _err.message();
+    } else if (linear) {
+      os <<
+        setfill(' ') << left << setw(MAX_UPC_LEN) << upc() << '|' <<
+        setw(20) << name() << '|' <<
+        right << setw(7) << fixed << showpoint << setprecision(2) << cost() << '|' <<
+        setw(4) << quantity() << '|' <<
+        setw(4) << qtyNeeded();
+    } else {
+      os <<
+        "upc: "              << upc()       << endl <<
+        "name: "             << name()      << endl <<
+        "price: "            << price()     << endl <<
+        "Price after tax: "  << cost()      << endl <<
+        "Quantity On Hand: " << quantity()  << endl <<
+        "Quantity Needed: "  << qtyNeeded() << endl;
+    }
+    return os;
+  }
 
+  istream& NFI::conInput(istream& is) {
+    char sbuf[256];
+    double dbuf;
+    int ibuf;
 
+    if (is.fail()==false) {
+      _err.clear();
+      cout << "Non-Food Item Entry: " << endl;
 
-  // istream& NFI::conInput(istream& istr)
+      cout << "Upc: ";
+      is >> sbuf;
+      upc(sbuf);
 
+      cout << "Name: ";
+      is >> sbuf;
+      name(sbuf);
 
+      cout << "Price: ";
+      is >> dbuf;
+      if (cin.fail()) {
+        _err.message("Invalid Price Entry");
+      } else {
+        price(dbuf);
+      }
+
+      if (_err.isClear()) {
+        cout << "Quantity On hand: ";
+        is >> ibuf;
+        if (cin.fail()) {
+          _err.message("Invalid Quantity Entry");
+        } else {
+          quantity(ibuf);
+        }
+      }
+
+      if (_err.isClear()) {
+        cout << "Quantity Needed: ";
+        is >> ibuf;
+        if (cin.fail()) {
+          _err.message("Invalid Quantity Needed Entry");
+        } else {
+          qtyNeeded(ibuf);
+        }
+      }
+    }
+
+    return is;
+  }
 
 }
--- FP20151MS5/NFI.h
+++ FP20151MS5/NFI.h
@@ -1,20 +1,39 @@
+// OOP244 Final Project Milestone 4, 20151
+// File NFI.h
+// Version 1.0
+// Date 2015/04/13
+// Author   Rafael Moraes
+// Description
+// Non-Food Item header file
+//
+// Revision History
+// -----------------------------------------------------------
+// Name         Date              Reason
+// R.Moraes     2015/04/13        Assignment completion
+////////////////////////////////////////////////////////////////
+
 #ifndef __244_NFI_H__
 #define __244_NFI_H__
 
-// includes go here
+#include "Item.h"
+#include "ErrorMessage.h"
 
+#define SBUF_LEN 256
 
+namespace oop244 {
 
-namespace oop244{
-  class NFI : public Item{
+  class NFI : public Item {
   private:
     ErrorMessage _err;
   public:
-    // default constructor
+    NFI();
 
-    // pure virutal method implementation prototypes
-
+    std::fstream& store(std::fstream& file) const;
+    std::fstream& load(std::fstream& file);
+    std::ostream& display(std::ostream& os, bool linear) const;
+    std::istream& conInput(std::istream& is);
 
   };
+
 }
 #endif
