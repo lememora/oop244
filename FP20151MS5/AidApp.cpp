0548d3bebd5a2cb7adb5131f3511d16006ac6ea2
Rafael Moraes <contact@moraesrafael.com>
2015-04-14 12:52:10 -0400

    compiling

--- FP20151MS5/AidApp.cpp
+++ FP20151MS5/AidApp.cpp
@@ -22,36 +22,38 @@ using namespace std;
 namespace oop244 {
 
   // copy constructor and assignment operator
+  AidApp::AidApp(const AidApp& A) {}
+  AidApp& AidApp::operator=(const AidApp& A) {
+    return *this;
+  }
 
-
-
-  // constructor 
+  // constructor
   // copies filename to _filename
   // sets all the _items to null
   // sets _noOfItems to null
   // loads the Records
-  AidApp::AidApp(const char* filename){
+  AidApp::AidApp(const char* filename) {
 
 
 
   }
 
-  // Menu() displays the menu as follows and waits for the user to 
-  // select an option. 
+  // Menu() displays the menu as follows and waits for the user to
+  // select an option.
   // if the selection is valid, it will return the selection
   // if not it will return -1
   // this funtion makes sure there is no characters left in keyboard
   // and wipes it before exit.
-  int AidApp::menu(){
+  int AidApp::menu() {
 
-
+    return -1;
 
   }
 
 
-  // lists all the items in linear format on the screen 
+  // lists all the items in linear format on the screen
 
-  void AidApp::listItems()const{
+  void AidApp::listItems() const {
 
 
 
@@ -63,7 +65,7 @@ namespace oop244 {
   // opens the file for writing
   // stores the items in the file
   // closes the file
-  void AidApp::saveRecs(){
+  void AidApp::saveRecs() {
 
 
 
@@ -73,11 +75,11 @@ namespace oop244 {
  // Opens the file for reading, if it does not exist, it will create an
  // empty file and exits otherwise :
  // Loads Records from the file overwriting the old ones pointed by item.
- // This function makes sure when loading records into _item pointers, they 
+ // This function makes sure when loading records into _item pointers, they
  // are deallocated before if they are already pointing to an item
  // closes the file
 
-  void AidApp::loadRecs(){
+  void AidApp::loadRecs() {
 
 
 
@@ -85,12 +87,12 @@ namespace oop244 {
   }
 
   // Searchers for the item in the _items array with the same UPC.
-  // if found, it will display the item in non-linear format and then 
+  // if found, it will display the item in non-linear format and then
   // asks for the value of the items purchased, and if the value does not exceed
   // the number needed to fulfill the requirement, it will update the quantity onhand
-  // value of the found record and then saves all the records to the file, overwriting 
+  // value of the found record and then saves all the records to the file, overwriting
   // the old values
-  void AidApp::updateQty(const char* UPC){
+  void AidApp::updateQty(const char* UPC) {
 
 
 
@@ -102,22 +104,22 @@ namespace oop244 {
   // searches for an item in the _items array for the same upc
   // if found it will return the index of the found item in _items
   // otherwise returns -1
-  int AidApp::SearchItems(const char* upc)const{
+  int AidApp::SearchItems(const char* upc) const {
 
-
+    return -1;
 
   }
 
 
   // creates a new item (NFI or Perishable) based on the argument
   // recieved dynamically, asks the user to fill in the values
-  // if the user fills the values with no mistake, it will open the file 
+  // if the user fills the values with no mistake, it will open the file
   // for writing, and then stores the item in the file.
-  // and print a proper message. 
+  // and print a proper message.
   // if the user makes a mistake (cin fails) it will only display the item
   // to show the error.
   // in any way it will delete the item before exiting the function
-  void AidApp::addItem(bool isPerishable){
+  void AidApp::addItem(bool isPerishable) {
 
 
 
@@ -131,8 +133,9 @@ namespace oop244 {
   //     from the file into the AidApp
   // 4, gets a UPC and then updates the quantity of it
   // 0, exits the program.
-  int AidApp::run(){
+  int AidApp::run() {
 
+    return 0;
 
   }
 }
--- FP20151MS5/AidApp.h
+++ FP20151MS5/AidApp.h
@@ -21,14 +21,15 @@ namespace oop244 {
 
   class AidApp {
     // private member arguments
-
-
+    char _filename[256];
+    Item* _items[MAX_NO_RECS];
+    std::fstream datafile;
+    int _noOfItems;
 
     // copy constructor and assingment operator
     // to prevent copying and assignment
-
-
-
+    AidApp(const AidApp& A);
+    AidApp& operator=(const AidApp& A);
 
     // private member fucntions
 
@@ -36,9 +37,15 @@ namespace oop244 {
 
   public:
     // constructor and run member function
-
-
-
+    AidApp(const char* filenamex);
+    int menu();
+    void listItems() const;
+    void saveRecs();
+    void loadRecs();
+    void updateQty(const char* UPC);
+    int SearchItems(const char* upc) const;
+    void addItem(bool isPerishable);
+    int run();
   };
 }
 
