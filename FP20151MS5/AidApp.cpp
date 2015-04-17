f10bfd649df5628f19c39eed0ef4c3ae5228ff24
Rafael Moraes <contact@moraesrafael.com>
2015-04-17 13:57:48 -0400

    implementation of class AidApp almost done

--- FP20151MS5/AidApp.cpp
+++ FP20151MS5/AidApp.cpp
@@ -38,10 +38,12 @@ namespace oop244 {
       if (strlen(filename) >= MAX_ISTR_LEN) {
         _filename[MAX_ISTR_LEN-1] = 0;
       }
-      _noOfItems = MAX_NO_RECS;
-      while (_noOfItems-- > 0) {
+
+      _noOfItems = MAX_NO_RECS-1;
+      do {
         _items[_noOfItems] = 0;
-      }
+      } while (_noOfItems-- > 0);
+
       loadRecs();
     }
   }
@@ -70,24 +72,24 @@ namespace oop244 {
 
   void AidApp::listItems() const {
     cout <<
-      left << setfill(' ') << setw(6) << "Row" << "|" <<
-      left << setfill(' ') << setw(8)  << "UPC" << "|" <<
-      left << setfill(' ') << setw(20) << "Item Name" << "|" <<
-      left << setfill(' ') << setw(7)  << "Cost" << "|" <<
+      left  << setfill(' ') << setw(6)  << "Row" << "|" <<
+      left  << setfill(' ') << setw(8)  << "UPC" << "|" <<
+      left  << setfill(' ') << setw(20) << "Item Name" << "|" <<
+      left  << setfill(' ') << setw(7)  << "Cost" << "|" <<
       right << setfill(' ') << setw(4)  << "QTY" << "|" <<
       right << setfill(' ') << setw(4)  << "Need" << "|" <<
-      left << setfill(' ') << setw(10) << "Unit" << "|" <<
+      left  << setfill(' ') << setw(10) << "Unit" << "|" <<
       right << setfill(' ') << setw(10) << "Expiry" <<
-      endl <<
-      left << setfill('-') <<
-      left << setfill(' ') << setw(5) << "|-" <<
-      left << setfill(' ') << setw(7) << "|-" <<
-      left << setfill(' ') << setw(19) << "|-" <<
-      left << setfill(' ') << setw(6) << "|" <<
-      right << setfill(' ') << setw(4) << "|" <<
-      left << setfill(' ') << setw(4) << "|-" <<
-      left << setfill(' ') << setw(9) << "|-" <<
-      left << setfill(' ') << setw(9) << endl;
+      endl  <<
+      left  << setfill('-') <<
+      left  << setfill(' ') << setw(5)  << "|-" <<
+      left  << setfill(' ') << setw(7)  << "|-" <<
+      left  << setfill(' ') << setw(19) << "|-" <<
+      left  << setfill(' ') << setw(6)  << "|" <<
+      right << setfill(' ') << setw(4)  << "|" <<
+      left  << setfill(' ') << setw(4)  << "|-" <<
+      left  << setfill(' ') << setw(9)  << "|-" <<
+      left  << setfill(' ') << setw(9)  << endl;
   }
 
 
@@ -95,9 +97,12 @@ namespace oop244 {
   // stores the items in the file
   // closes the file
   void AidApp::saveRecs() {
-
-
-
+    int c = 0;
+    datafile.open(_filename, ios::out);
+    do {
+      _items[c]->store(datafile);
+    } while (c++ < MAX_NO_RECS);
+    datafile.close();
   }
 
 
@@ -109,10 +114,38 @@ namespace oop244 {
  // closes the file
 
   void AidApp::loadRecs() {
+    int readIndex = 0;
+    char c;
 
+    datafile.open(_filename, ios::in);
 
+    if (datafile.fail()) {
+      datafile.clear();
+      datafile.close();
+      datafile.open(_filename, ios::out);
+    } else {
+      while (datafile.fail() == false) {
+        c = 0;
+        if (_items[readIndex] != ((Item*)0)) {
+          delete _items[readIndex];
+          _items[readIndex] = 0;
+        }
+        if (datafile >> c && !datafile.fail()) {
+          if (c=='P') {
+            _items[readIndex] = new NFI();
+          } else if (c=='N') {
+            _items[readIndex] = new Perishable();
+          }
+        }
+        if (_items[readIndex] != ((Item*)0)) {
+          datafile >> c; // skip comma
+          _items[readIndex++]->load(datafile);
+        }
+      }
+    }
 
-
+    _noOfItems = readIndex;
+    datafile.close();
   }
 
   // Searchers for the item in the _items array with the same UPC.
@@ -122,11 +155,28 @@ namespace oop244 {
   // value of the found record and then saves all the records to the file, overwriting
   // the old values
   void AidApp::updateQty(const char* UPC) {
-
-
-
-
-
+    int c=SearchItems(UPC);
+    int q, r = 0;
+    if (c == -1) {
+      cout << "Not found!" << endl;
+    } else {
+      _items[c]->display(cout, false);
+      cout << endl << "Please enter the number of purchased items: ";
+      cin >> q;
+      if (cin.fail()) {
+        cout << "Invalid Quantity value!" << endl;
+      } else {
+        r = _items[c]->qtyNeeded() - _items[c]->quantity();
+        if (q <= r) {
+          _items[c] += q;
+        } else {
+          _items[c] += r;
+          cout << "Too much items, only " << r << " is needed, please return the extra " << (q+r) << " items." << endl;
+        }
+        saveRecs();
+        cout << "Updated!" << endl;
+      }
+    }
   }
 
 
@@ -134,9 +184,9 @@ namespace oop244 {
   // if found it will return the index of the found item in _items
   // otherwise returns -1
   int AidApp::SearchItems(const char* upc) const {
-
-    return -1;
-
+    int c = 0;
+    while (((*_items[c])==upc)==false && c++ < _noOfItems);
+    return c == _noOfItems ? -1 : c;
   }
 
 
@@ -149,10 +199,19 @@ namespace oop244 {
   // to show the error.
   // in any way it will delete the item before exiting the function
   void AidApp::addItem(bool isPerishable) {
+    Item* i;
+    if (isPerishable) i = new Perishable();
+    else              i = new NFI();
 
-
-
-
+    cin >> *i;
+    if (cin.fail()) {
+      i->display(cout, false);
+    } else {
+      datafile.open(_filename, ios::out);
+  		i->store(datafile);
+      datafile.close();
+  		cout << "Item added" << endl;
+    }
   }
 
   // diplays the menu and receives the user selection
@@ -163,8 +222,29 @@ namespace oop244 {
   // 4, gets a UPC and then updates the quantity of it
   // 0, exits the program.
   int AidApp::run() {
+    int n = 0;
+    char u[MAX_UPC_LEN];
 
+    while ((n=menu()) != 0) {
+      if (n==1) {
+        listItems();
+      } else if (n==2) {
+        addItem(false);
+        loadRecs();
+      } else if (n==3) {
+        addItem(true);
+        loadRecs();
+      } else if (n==4) {
+				cout << "Please enter the UPC: ";
+				cin >> u;
+				updateQty(u);
+      } else {
+        cout << "==Invalid Selection, try again.===";
+  			cout << endl;
+      }
+    }
+
+    cout << "Goodbye!!" << endl;
     return 0;
-
   }
 }
